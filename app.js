0eb7bbef7dfbfe9fb4b62e7d578a1df409ce1e7a
RoniMl <ronimalamud@gmail.com>
2023-06-08 16:03:55 -0300

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
-const path = require("path")
+const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(3000,() => console.log("Servidor Corriendo"));
+app.get("/register.html", function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/register.html');
+    res.sendFile(htmlPath);
+  });
+  
+  app.get("/signup.html", function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/signup.html');
+    res.sendFile(htmlPath);
+  });
 
+app.get("/login.html", function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/login.html');
+    res.sendFile(htmlPath);
+  });
 
+app.get("/",function(req,res) {
+    let htmlPath = path.resolve(__dirname,'./views/index.html')
+    res.sendFile (htmlPath);
+    })
 
-app.get("/",(req,res) => {
-
-   let htmlPath = path.resolve(__dirname,"./views/index.html")
-   res.sendFile(htmlPath)
-})
-
+app.listen(3000, function () {
+    console.log("Servidor corriendo");
+});
